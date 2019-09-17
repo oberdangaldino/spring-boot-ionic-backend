c2c2b8f6be21fd7da7e3de0d43a0025b678f31a4
Oberdan Galdino <oberdanrg@gmail.com>
2019-09-17 14:03:32 -0300

    Endpoint /pedido/{id} disponibilizado

---
+++ src/main/java/com/oberdangaldino/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.oberdangaldino.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oberdangaldino.cursomc.domain.Pedido;
+import com.oberdangaldino.cursomc.repositories.PedidoRepository;
+import com.oberdangaldino.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
